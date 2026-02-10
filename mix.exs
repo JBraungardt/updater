60eeb81dd947ad2beacbcaa0306aae69ff8ea860
Juergen Braungardt <Juergen.Braungardt@mevis.de>
2026-02-10 16:55:41 +0100

    add credo

--- mix.exs
+++ mix.exs
@@ -20,6 +20,8 @@ defmodule Updater.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
   end
 end
